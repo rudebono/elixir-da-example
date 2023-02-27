c52f7946e1f51b816d2da6c048b358b607b09b85
Hany Rock <rock@momenti.tv>
2023-02-27 09:50:16 +0900

    init ml app

---
+++ apps/hany_ml/lib/hany_ml.ex
@@ -0,0 +1,18 @@
+defmodule HanyMl do
+  @moduledoc """
+  Documentation for `HanyMl`.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> HanyMl.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ apps/hany_ml/test/hany_ml_test.exs
@@ -0,0 +1,8 @@
+defmodule HanyMlTest do
+  use ExUnit.Case
+  doctest HanyMl
+
+  test "greets the world" do
+    assert HanyMl.hello() == :world
+  end
+end
